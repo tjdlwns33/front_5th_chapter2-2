770f3772e1c842ddca9c2f0d0145a2f60bc45ebc
tjdlwns33 <tjdlwns33@gmail.com>
2025-04-25 00:32:42 +0900

    배포준비

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    root: "./", // 루트 디렉터리 설정
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.refactoring.html", // 사용하고자 하는 HTML 파일로 경로 지정
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
